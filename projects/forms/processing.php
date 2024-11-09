1e88a2a61ed96676448e56c4b12ffe32edd68e77
Joseph Purcell <jopurcell24@gmail.com>
2024-11-09 09:00:37 -1000

    php file

---
+++ projects/forms/processing.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Signed Up</title>
+</head>
+<body>
+    <main>
+        <h1>Your account has been created. Thank you!</h1>
+    </main>
+</body>
+</html>
