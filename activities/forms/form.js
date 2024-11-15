001380e3f59c81d1662e5315aeeaa8f36f479e15
Joseph Purcell <jopurcell24@gmail.com>
2024-11-15 11:48:25 -1000

    activity

---
+++ activities/forms/form.js
@@ -0,0 +1,31 @@
+document.getElementById('myForm').addEventListener('submit',function(event) {
+    event.preventDefault();
+    const firstname = document.getElementById('fname').value;
+    const lastname = document.getElementById('lname').value;
+    
+    const formData = {
+        firstname: firstname,
+        lastname: lastname
+    }
+
+    if (!firstname || !lastname) {
+        alert('You must give a first and last name')
+        return;
+    }
+
+    const xhr = XMLHttpRequest();
+    xhr.open("POST", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readystate === 4 && xhr.status === 200) {
+            message = JSON.parse(xhr.responseText)
+            document.getElementById('message').innerHTML = response.message;
+            document.getElementById('myForm').innerHTML = "";
+        } else if (xhr.readystate === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+
+    console.log(formData)
+})
