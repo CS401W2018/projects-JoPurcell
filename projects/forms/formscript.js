ebf73dec1fe0ecb83ae32416916314501fb42a0c
Joseph Purcell <jopurcell24@gmail.com>
2024-11-17 20:26:39 -1000

    changes to project

--- projects/forms/formscript.js
+++ projects/forms/formscript.js
@@ -5,13 +5,6 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
     const age = document.getElementById('age').value;
     const password = document.getElementById('pass').value;
     
-    const formData = {
-        firstname: firstname,
-        lastname: lastname,
-        password: document.getElementById('pass').value,
-        state: document.getElementById('state').value
-    };
-
     if (!firstname || !lastname) {
         alert('You must give a first and last name');
         return;
@@ -27,18 +20,25 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
         return;
     }
 
+    const formData = {
+        firstname: firstname,
+        lastname: lastname,
+        password: document.getElementById('pass').value,
+        state: document.getElementById('state').value
+    };
+
     const xhr = new XMLHttpRequest();
     xhr.open("GET", "submit.json", true);
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
-        if (xhr.readystate === 4 && xhr.status === 200) {
+        if (xhr.readyState === 4 && xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
             document.getElementById("message").innerHTML = response.message;
             document.getElementById("myForm").innerHTML = "";
-        } else if (xhr.readystate === 4) {
+        } else if (xhr.readyState === 4) {
             alert('Error submitting form.');
         }
     };
     xhr.send(JSON.stringify(formData));
     console.log(formData);
-})
+});
