e645aae376600db6e30cfead340c8a2f21a6d5a2
Joseph Purcell <jopurcell24@gmail.com>
2024-11-30 11:08:49 -1000

    forms

---
+++ projects/finalproject/formscript.js
@@ -0,0 +1,44 @@
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const firstname = document.getElementById('fname').value;
+    const lastname = document.getElementById('lname').value;
+    const age = document.getElementById('age').value;
+    const password = document.getElementById('pass').value;
+    
+    if (!firstname || !lastname) {
+        alert('You must give a first and last name');
+        return;
+    }
+
+    if (!age || age < 18) {
+        alert('You must be 18 or older to submit this form');
+        return;
+    }
+
+    if (!password) {
+        alert('Please enter a valid password.');
+        return;
+    }
+
+    const formData = {
+        firstname: firstname,
+        lastname: lastname,
+        password: password,
+        state: document.getElementById('state').value
+    };
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", "submit.json", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = "";
+        } else if (xhr.readyState === 4) {
+            alert('Error submitting form.');
+        }
+    };
+    xhr.send(JSON.stringify(formData));
+    console.log(formData);
+});
